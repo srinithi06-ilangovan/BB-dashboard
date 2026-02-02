f52d5e341fb8647c1bb145558dc2ad99ae72d5bf
srinithi.ilangovan <srinithi.ilangovan@ticketmaster.com>
2026-02-02 20:05:47 +0530

    added data for 2026 q1

--- bb-client-SRC/src/utils/jqlHelper.js
+++ bb-client-SRC/src/utils/jqlHelper.js
@@ -7,13 +7,15 @@ export const pod = {
         Q1_2025: 'e.subbiah, Anushree.Chandrasek, Soujanya.Shetty, sumitha.kumari, Sai.Suluru, praveer.nair, srinithi.ilangovan',
         Q2_2025: 'e.subbiah, Anushree.Chandrasek, Soujanya.Shetty, sumitha.kumari, praveer.nair, srinithi.ilangovan',
         Q3_2025: 'e.subbiah, Anushree.Chandrasek, Soujanya.Shetty, sumitha.kumari, sravanthi.vettigunta, praveer.nair, srinithi.ilangovan, g.chinniraviteja',
-        Q4_2025: 'e.subbiah, Anushree.Chandrasek, Soujanya.Shetty, sumitha.kumari, sravanthi.vettigunta, praveer.nair, srinithi.ilangovan, g.chinniraviteja',
+        Q4_2025: 'e.subbiah, Anushree.Chandrasek, Soujanya.Shetty, sumitha.kumari, sravanthi.vettigunta, praveer.nair, srinithi.ilangovan, ahana.mukherjee, harini.bhamidipati',
+        Q1_2026: 'e.subbiah, Anushree.Chandrasek, Soujanya.Shetty, sumitha.kumari, sravanthi.vettigunta, praveer.nair, srinithi.ilangovan, ahana.mukherjee, harini.bhamidipati, murali.krishna, dilip.mudduluru'
     },
     Orion: {
         Q1_2025: 'saranraj.balu, r.nagaimuthu, kanishka.das, Dhwanit.Kumar, shivam.patwal, swetha.busam, chirag.chhabria, karthik.gunti, vivekprabhakaran.j',
         Q2_2025: 'saranraj.balu, kanishka.das, Dhwanit.Kumar, shivam.patwal, swetha.busam, chirag.chhabria, karthik.gunti, vivekprabhakaran.j',
         Q3_2025: 'saranraj.balu, sathish.kumarmohan, namrata.chakraborty, shilpa.kanojiya, chirag.chhabria, swetha.busam, karthik.gunti, vivekprabhakaran.j',
-        Q4_2025: 'saranraj.balu, sathish.kumarmohan, namrata.chakraborty, shilpa.kanojiya, chirag.chhabria, swetha.busam, karthik.gunti, vivekprabhakaran.j',
+        Q4_2025: 'saranraj.balu, sathish.kumarmohan, namrata.chakraborty, shilpa.kanojiya, swetha.busam, karthik.gunti, vivekprabhakaran.j, N.Weldegiorgis, aravind.rajendran',
+        Q1_2026: 'saranraj.balu, sathish.kumarmohan, namrata.chakraborty, shilpa.kanojiya, swetha.busam, karthik.gunti, N.Weldegiorgis, aravind.rajendran',
     },
     DigitalPenny: {
         Q1_2025: 'v.avva, deepa.lingeswari, priya.tripathi, tanmy.rastogi, m.muramshetty, Mugilan.ka, rahul.tomar, Rocky.Sinha',
@@ -23,6 +25,7 @@ export const pod = {
         Q1_2025: 'susha.raghunath, shweta.padhy, harsha.kanthi, kishore.johnsan, Manisha.Kundena, karthick.p2, jaiprakash.madhavan',
         Q2_2025: 'susha.raghunath, shweta.padhy, harsha.kanthi, kishore.johnsan, Manisha.Kundena, karthick.p2, jaiprakash.madhavan',
         Q3_2025: 'v.avva, shweta.padhy, harsha.kanthi, kishore.johnsan, Manisha.Kundena, karthick.p2, jaiprakash.madhavan',
-        Q4_2025: 'v.avva, shweta.padhy, harsha.kanthi, kishore.johnsan, Manisha.Kundena, karthick.p2, jaiprakash.madhavan'
+        Q4_2025: 'v.avva, shweta.padhy, harsha.kanthi, kishore.johnsan, Manisha.Kundena, karthick.p2, jaiprakash.madhavan',
+        Q1_2026: 'v.avva, shweta.padhy, harsha.kanthi, kishore.johnsan, Manisha.Kundena, karthick.p2, jaiprakash.madhavan'
     }
 }
